d9e9ab8d43781d4522d697ee1cb00602e90eded9
Eric Wollesen <eric@leadtune.com>
2012-06-12 10:51:20 -0600

    Use the hudson REST API.

--- rg.rb
+++ rg.rb
@@ -1,93 +1,49 @@
 #!/usr/bin/env ruby
 
-require "nokogiri"
-require "open-uri"
+require "json"
+require "yaml"
 require "time"
+require "open-uri"
 require "colorize"
-require "ruby-debug"
 
-
-def first_word(title)
-  title.gsub(/\s.*/, "")
-end
-
-def render_pass(title, date)
-  puts "PASS:".colorize(:green) + " #{first_word(title)}"
-end
-
-def render_fail(title, date)
-  puts "FAIL:".colorize(:red) + " #{first_word(title)} (as of #{date})"
-end
-
-def print_heading(heading, out=$stdout)
-  out.puts
-  out.puts "=" * heading.size
-  out.puts heading
-  out.puts "=" * heading.size
-end
-
-def title_indicates_passing?(title)
-  passing_re = [/\(stable\)$/,
-                /\(back to normal\)$/]
-  passing_re.any? {|re| re === title}
-end
-
-def title_indicates_failure?(title)
-  failing_re = [/\(broken since[^\)]+\)$/]
-  failing_re.any? {|re| re === title}
-end
-
-def title_indicates_building?(title)
-  building_re = [/\(?\)$/]
-  building_re.any? {|re| re === title}
-end
-
-def previous_build_successful?(entry)
-  url = "http" + entry.at("id").text.split("http").last + "rssAll"
-
-  Nokogiri.XML(open(url)).search("entry").each do |e|
-    title = entry.at("title").text
-
-    return true if title_indicates_passing?(title)
-    return false if title_indicates_failure?(title)
-  end
-
-  false
-end
-
-def determine_if_passing?(entry)
-  title = entry.at("title").text
-
-  title_indicates_passing?(title) ||
-    (title_indicates_building?(title) && previous_build_successful?(entry))
-end
 
 def check_server(server_record)
-  name, url = server_record
+  server_name, base_url = server_record
+  url = URI.parse(base_url)
 
-  Nokogiri.XML(open(url)).search("entry").inject([]) do |e, i|
-    title = i.at("title").text
-    date = Time.parse(i.at("updated").text)
-    passing = determine_if_passing?(i)
+  url.path = "/rest/projects"
+  json = JSON.load(open(url))
 
-    e << [name, title, date, passing,]
+  json["projects"].inject([]) do |e, project|
+    e << [server_name, *process_project(project)]
   end
 end
 
-if $0 == __FILE__
-  servers = {
-    "Sprint" => "http://ci-sprint.lmpcloud.com:8080/rssLatest",
-    "Production" => "http://ci-production.lmpcloud.com:8080/rssLatest",
-  }
-  passing, failing = servers.inject([]) do |l, server|
-    l += check_server(server)
-  end.partition {|r| r[3]}
+def process_project(project)
+  timestamp = Time.at(project["lastBuild"]["timeStamp"] / 1000)
+  passing = project["lastBuild"]["result"] == "SUCCESS"
+  [project["title"], timestamp.ctime, passing]
+end
 
+def render_report(passing, failing)
   puts "#{passing.size}".colorize(:green) + " builds passing"
   unless failing.empty?
     puts "#{failing.size}".colorize(:red) + " builds failing"
   end
   failing.sort_by {|x| x.map(&:to_s).map(&:downcase)}.each do |(server, title, date, _)|
-    puts "%-11s %-40s\t%s" % ["#{server}:", first_word(title).colorize(:red), date.localtime]
+    puts "%-11s %-40s\t%s" % ["#{server}:", title.colorize(:red), date]
   end
+end
+
+if $0 == __FILE__
+  servers = {
+    "Sprint" => "http://ci-sprint.lmpcloud.com:8080/",
+    "Production" => "http://ci-production.lmpcloud.com:8080/",
+  }
+  servers = YAML.load(open(File.expand_path("~/.rg.yaml")))
+  passing, failing = servers.inject([]) do |l, server|
+    l += check_server(server)
+  end.partition {|r| r[3]}
+
+  render_report(passing, failing)
 end
